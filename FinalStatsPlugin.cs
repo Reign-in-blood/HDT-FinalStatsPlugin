94f3d6c0561beb2ab97074fe750f7891f80d98a3
Reign-in-blood <canni.gaming@gmail.com>
2026-09-14 17:09:43 +0200

    chore: bump plugin version to 0.1.56

--- FinalStatsPlugin.cs
+++ FinalStatsPlugin.cs
@@ -34,7 +34,7 @@ namespace FinalStatsPlugin
 
         public string ButtonText => "Show / hide";
         public string Author => "Benito";
-        public Version Version => new Version(0, 1, 55);
+        public Version Version => new Version(0, 1, 56);
         public MenuItem MenuItem => null;
 
         // ------------------------------------------------------------
